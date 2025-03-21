aea0d2028a29c29ea33420acec23acb30d279bfe
shizis <shizilnx@gmail.com>
2025-03-21 16:14:21 +0700

    link share update fix

--- resources/views/livewire/create-guess.blade.php
+++ resources/views/livewire/create-guess.blade.php
@@ -44,11 +44,10 @@
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                     </svg>
-
                                 </button>
                             </form>
                             <a data-action="share/whatsapp/share"
-                                href="https://api.whatsapp.com/send?text=Assalamualaikum Warahmatullahi Wabarakatuh%0a%0aTanpa mengurangi rasa hormat, perkenankan kami mengundang Bapak/Ibu/Saudara/i {{ $gues->name }} untuk menghadiri acara kami %0a%0a{{ url("/angga-laras/{$gues->slug}") }}%0a%0a"
+                                href="https://api.whatsapp.com/send?text=Assalamualaikum Warahmatullahi Wabarakatuh.%0a%0aTanpa mengurangi rasa hormat, perkenankan kami mengundang Bapak/Ibu/Saudara/i *{{ $gues->name }}* untuk menghadiri acara kami %0a%0aBerikut link undangan kami, untuk info lengkap dari acara bisa kunjungi:%0a {{ url("/angga-laras/{$gues->slug}") }}%0a%0aMerupakan suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan untuk hadir dan memberikan doa restu.%0a%0aMohon maaf perihal undangan hanya di bagikan melalui pesan ini.%0a%0aTerima kasih banyak atas perhatiannya.%0a%0aWassalamualaikum Warahmatullahi Wabarakatuh."
                                 class="bg-green-500 px-4 py-2 text-sm rounded-md hover:bg-green-600 text-white font-semibold">
                                 <img src="{{ url('images/assets/WhatsApp_icon.png') }}" alt="whatsapp"
                                     class="size-6 object-fill object-center">
